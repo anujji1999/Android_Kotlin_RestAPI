762594ce6d7a50620c38095cc13840377db617f9
Arnav Gupta <arnav@codingblocks.com>
2018-01-14 14:33:43 +0530

    using extension functions
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- app/src/main/java/com/codingblocks/restapiconsumer/activities/PostsActivity.kt
+++ app/src/main/java/com/codingblocks/restapiconsumer/activities/PostsActivity.kt
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.codingblocks.restapiconsumer.App
 import com.codingblocks.restapiconsumer.Post
+import com.codingblocks.restapiconsumer.extensions.enqueue
 import com.codingblocks.restapiconsumer.rfcb
 import org.jetbrains.anko.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
@@ -22,13 +23,11 @@ class PostsActivity : AppCompatActivity(), AnkoLogger {
         ui.setContentView(this)
         ui.rvPosts.backgroundColor = Color.RED
 
-        (application as App).getApi().getPosts().enqueue(rfcb({
+        (application as App).getApi().getPosts().enqueue({
             t, r ->
-            t?.let { warn("Posts not fetched", it) }
-            r?.let {
-                info("Posts = " + it.body()?.size)
-            }
-        }))
+            t?.let { warn("Could not fetch posts", t)}
+            r?.let { info("Posts = " + it.body()?.size) }
+        })
     }
 
     class PostsActivityUI: AnkoComponent<PostsActivity> {
--- app/src/main/java/com/codingblocks/restapiconsumer/extensions/Retrofit.kt
+++ app/src/main/java/com/codingblocks/restapiconsumer/extensions/Retrofit.kt
@@ -1,5 +1,21 @@
 package com.codingblocks.restapiconsumer.extensions
 
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 /**
  * Created by championswimmer on 14/01/18.
  */
+fun <T> Call<T>.enqueue (cb: (t: Throwable?, r: Response<T>?) -> Unit) {
+    enqueue(object: Callback<T> {
+        override fun onResponse(call: Call<T>?, response: Response<T>?) {
+            cb(null, response)
+        }
+
+        override fun onFailure(call: Call<T>?, t: Throwable?) {
+            cb(t, null)
+        }
+
+    })
+}
