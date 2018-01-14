b70347ed6193fbc4691d5e6bb2c33ebd308a5645
Arnav Gupta <arnav@codingblocks.com>
2018-01-14 13:41:58 +0530

    first commit
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ app/src/main/java/com/codingblocks/restapiconsumer/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.codingblocks.restapiconsumer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.codingblocks.restapiconsumer.activities.PostsActivity
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    class MainAcivityUI: AnkoComponent<MainActivity> {
+        override fun createView(ui: AnkoContext<MainActivity>): View = with(ui) {
+            verticalLayout {
+                lparams(width = matchParent, height = matchParent)
+
+                linearLayout {
+                    lparams(width = matchParent, height = 0, weight = 1F)
+                    button("POSTS").lparams(
+                            height = matchParent,
+                            width = 0,
+                            weight = 1F
+                    ).onClick { startActivity<PostsActivity>() }
+
+                    button("ALBUMS")
+                            .lparams(
+                                    height = matchParent,
+                                    width = 0,
+                                    weight = 1F
+                            )
+                }
+                linearLayout {
+                    lparams(width = matchParent, height = 0, weight = 1F)
+                    button("USERS")
+                            .lparams(
+                                    height = matchParent,
+                                    width = 0,
+                                    weight = 1F
+                            )
+                    button("TODOS")
+                            .lparams(
+                                    height = matchParent,
+                                    width = 0,
+                                    weight = 1F
+                            )
+                }
+            }
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/codingblocks/restapiconsumer/activities/PostsActivity.kt
@@ -0,0 +1,11 @@
+package com.codingblocks.restapiconsumer.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PostsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
